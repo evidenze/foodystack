f9f7b91046ac66f63078aa83c5dcaa0464a73e96
Evidenze <evidenzeekanem@gmail.com>
2019-11-12 14:31:42 +0100

    Update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/welcome') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -49,10 +49,10 @@
                                 <a class="nav-link" href="{{ url('/home') }}">{{ __('Home') }}</a>
                             </li>                            
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('showCart') }}">{{ __('About Us') }}</a>
+                                <a class="nav-link" href="#">{{ __('About Us') }}</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('showCart') }}">{{ __('Contact Us') }}</a>
+                                <a class="nav-link" href="#">{{ __('Contact Us') }}</a>
                             </li>
                         @guest
                             <li class="nav-item">
