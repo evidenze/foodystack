eaa0ab291d608930f71796e0b034fa0bdbab43ed
Evidenze <evidenzeekanem@gmail.com>
2019-11-12 17:03:30 +0100

    Update

--- resources/views/greet.blade.php
+++ resources/views/greet.blade.php
@@ -17,18 +17,19 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=McLaren&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=DM+Sans:400,700&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ url('css/main.css') }}" rel="stylesheet">
 </head>
-<body style="background:#5e35b1;">
+<body style="background:#5e35b1;font-family: 'DM Sans', sans-serif;">
     <div id="app">
        <div class="container mt-5 text-center">
            <h1 class="text-white text-uppercase font-weight-bold" style="padding-top:100px;line-height:1.7;">Online Food Ordering <br>and Delivery System<br>By<br>Ubong Sylvanus Akpan</h1>
-           <p class="text-white">This platform is branded as FoodyStack.</p><br>
-           <a href="{{ url('/welcome') }}" class="btn greet">Visit Site</a>&nbsp;
-           <a target="_blank" href="https://github.com/evidenze/foodystack" class="btn greet">View Source Code</a>
+           <p class="text-white">This project is branded as FoodyStack.</p><br>
+           <a href="{{ url('/welcome') }}" class="btn greet font-weight-bold text-uppercase">Visit Site</a>&nbsp;
+           <a target="_blank" href="https://github.com/evidenze/foodystack" class="btn greet font-weight-bold text-uppercase">View Source Code</a>
        </div>
     </div>
 </body>
